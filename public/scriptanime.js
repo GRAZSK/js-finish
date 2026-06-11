641b0787fac033a9916f5723bd166a81c3a9adc6
Angelina <erorrforever@gmail.com>
2026-06-11 22:52:09 +1000

    переделала телик

---
+++ public/scriptanime.js
@@ -0,0 +1,50 @@
+var trailLength = 8;
+var path = "./pic/cursor.gif";
+
+var images = [];
+var storage = [];
+var d = 0;
+
+function initTrail() {
+  // Создаём элементы
+  for (var i = 0; i < trailLength; i++) {
+    var img = document.createElement("img");
+    img.src = path;
+    img.style.position = "absolute";
+    img.style.zIndex = "9999";
+    img.style.pointerEvents = "none"; // чтобы не мешал кликам
+    document.body.appendChild(img);
+    images.push(img);
+  }
+  
+  // Подготавливаем хранилище координат
+  for (var i = 0; i < trailLength * 2; i++) {
+    storage.push(0);
+  }
+  
+  trail();
+}
+
+function trail() {
+  for (var i = 0; i < images.length; i++) {
+    images[i].style.top = storage[d] + "px";
+    images[i].style.left = storage[d + 1] + "px";
+    d += 2;
+  }
+  
+  // Сдвигаем координаты
+  for (var i = storage.length - 1; i >= 2; i--) {
+    storage[i] = storage[i - 2];
+  }
+  d = 0;
+  
+  setTimeout(trail, 10);
+}
+
+function processEvent(e) {
+  storage[0] = e.clientY + window.scrollY + 10;
+  storage[1] = e.clientX + window.scrollX + 10;
+}
+
+document.addEventListener("mousemove", processEvent);
+window.addEventListener("load", initTrail);
